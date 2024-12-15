ac3f9eea109d88997540a69e4517403043de752a
Bekbolot <burxanovbeko@gmail.com>
2024-12-15 16:44:03 +0600

    first commit vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,13 @@
-<template><div clas>hi guys</div></template>
+<script setup>
+import router from "./router/inde";
+import Header from "./components/Header.vue";
+import Liquidation from "./components/Liquidation.vue";
+</script>
+<template>
+  <div class="">
+    <Header />
+    <!-- <Liquidation /> -->
+  </div>
+</template>
+
+<style scoped></style>
